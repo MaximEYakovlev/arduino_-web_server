12e7c66263be5444ef18b9dbac0300b6d9ae4d13
Maxim Yakovlev <it.trainee@yandex.ru>
2024-10-07 23:27:10 +0300

    refactoring: separation of server logic and request handling

--- WebServer.ino
+++ WebServer.ino
@@ -1,122 +1,130 @@
 /*
  Web Server
 
- A simple web server that shows the value of the analog input pins.
+ A simple web server that shows the value of the analog input pins
  using an Arduino WIZnet Ethernet shield.
 
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
  * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
- 
  */
 
 #include <SPI.h>
 #include <Ethernet.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
+// Constants
+const int ANALOG_PINS = 6;  // Number of analog pins
+
+// Ethernet configuration
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 177);
 
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
+// Initialize the Ethernet server on port 80
 EthernetServer server(80);
 
-void setup() {
-  // You can use Ethernet.init(pin) to configure the CS pin
-  Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH Shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit FeatherWing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit FeatherWing Ethernet
+// Function to initialize Ethernet and start the server
+void setupEthernet() {
+  // Initialize Ethernet with CS pin 10
+  Ethernet.init(10);
 
-  // Open serial communications and wait for port to open:
+  // Start serial communication for debugging
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ; // Wait for serial port to connect (only needed for some boards)
   }
-  Serial.println("Ethernet WebServer Example");
+  Serial.println("Starting Ethernet WebServer...");
 
-  // start the Ethernet connection and the server:
+  // Start Ethernet with static IP
   Ethernet.begin(mac, ip);
 
   // Check for Ethernet hardware present
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    Serial.println("Ethernet shield not found. Stopping execution.");
     while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
+      delay(1); // Infinite loop if Ethernet is not present
     }
   }
+
+  // Check if Ethernet cable is connected
   if (Ethernet.linkStatus() == LinkOFF) {
     Serial.println("Ethernet cable is not connected.");
   }
 
-  // start the server
+  // Start the server
   server.begin();
-  Serial.print("server is at ");
+  Serial.print("Server is running at IP: ");
   Serial.println(Ethernet.localIP());
 }
 
+// Function to send HTTP response to the client
+void sendHttpResponse(EthernetClient &client) {
+  // Send HTTP response headers
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close");  // Close connection after response
+  client.println("Refresh: 5");         // Refresh the page every 5 seconds
+  client.println();
 
-void loop() {
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an HTTP request ends with a blank line
-    bool currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the HTTP request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard HTTP response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
+  // Send HTML content
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  for (int analogChannel = 0; analogChannel < ANALOG_PINS; analogChannel++) {
+    int sensorReading = analogRead(analogChannel);
+    client.print("Analog input ");
+    client.print(analogChannel);
+    client.print(" is ");
+    client.print(sensorReading);
+    client.println("<br />");
+  }
+  client.println("</html>");
+}
+
+// Function to handle client connections
+void handleClient(EthernetClient &client) {
+  Serial.println("New client connected");
+
+  bool currentLineIsBlank = true;
+
+  // Read the client's request
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+
+      // If end of HTTP request (blank line), send response
+      if (c == '\n' && currentLineIsBlank) {
+        sendHttpResponse(client);
+        break;
+      }
+
+      // Check if the current line is blank
+      if (c == '\n') {
+        // Start of a new line
+        currentLineIsBlank = true;
+      } else if (c != '\r') {
+        // Current line has content
+        currentLineIsBlank = false;
       }
     }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
+  }
+
+  // Short delay to ensure data is sent
+  delay(1);
+
+  // Close the connection
+  client.stop();
+  Serial.println("Client disconnected");
+}
+
+void setup() {
+  setupEthernet();
+}
+
+void loop() {
+  // Check for incoming clients
+  EthernetClient client = server.available();
+
+  if (client) {
+    handleClient(client);
   }
 }
